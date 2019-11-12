e5e01e78265d97d36cf84fe2e5ea0c3ddb02d240
Florida Elago <florida.elago@gmail.com>
2019-11-12 14:51:54 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const word = document.getElementById("wordgen");
+
+const words = [
+  "Asphyxiation",
+  "Affiliation",
+  "Retaliation",
+  "Decentralisation",
+  "Fertilization",
+  "Accommodation",
+  "Dehydration"
+];
+
+const randomWord = frm => {
+  var rnd = Math.ceil(Math.random() * frm.length - 1);
+  return words[rnd];
+};
+
+function generateWords() {
+  word.innerText = randomWord(words);
+}
+
+generateWords();
